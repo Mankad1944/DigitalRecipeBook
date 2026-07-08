da18b43a527e9224d89f84ad036c83c0ea793d97
Mankad1944 <vanshil.mankad@yahoo.com>
2026-07-08 19:12:33 +0530

    Fix static files for Render deployment

--- digital_recipe_book/settings.py
+++ digital_recipe_book/settings.py
@@ -135,12 +135,20 @@ USE_TZ = True
 # STATIC_URL = '/templates/static/'
 # STATICFILES_DIRS = [BASE_DIR/'templates/static']
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_URL = "/static/"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage"
-)
+# STATICFILES_STORAGE = (
+#     "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# )
+
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
